2ba0a777c50b989883d8c939f9461c98fc9361cd
Aditya Rajput <adiraj20072002@gmail.com>
2023-07-28 16:31:15 +0530

    feat: Add `update` method and a backup system

--- firefox_bookmarks/__init__.py
+++ firefox_bookmarks/__init__.py
@@ -1,11 +1,14 @@
 import os
+import shutil
 from tempfile import gettempdir
-from typing import Iterable
+from time import time
+from typing import Any, Iterable
 
 from peewee import JOIN, DoesNotExist, Expression, Field, Model, ModelAlias, fn
 
 from .bookmark import Bookmark, connect_bookmark_model
 from .constants import BATCH_SIZE, BOOKMARK_TYPE, FOLDER_TYPE, ProfileCriterion
+from .locate import locate_db
 from .models import FirefoxBookmark, FirefoxOrigin, FirefoxPlace, connect_firefox_models
 
 FieldOrModel = Model | ModelAlias | Field
@@ -17,82 +20,162 @@ class FirefoxBookmarks:
         self._db_path = os.path.join(gettempdir(), 'bookmarks.sqlite')
 
         self._TRANSLATION = {
-            "FROM": (
-                FirefoxBookmark.id,
-                FirefoxBookmark.title,
-                FirefoxPlace.url,
-                FirefoxPlace.description,
-                FirefoxBookmark.type,
-                FirefoxBookmark.parent,
-                FirefoxBookmark.fk,
-                FirefoxPlace.origin,
-                FirefoxBookmark.date_added,
-                FirefoxBookmark.folder_type,
-                FirefoxPlace.foreign_count,
-                FirefoxBookmark.guid,
-                FirefoxPlace.hidden,
-                FirefoxBookmark.keyword_id,
-                FirefoxBookmark.last_modified,
-                FirefoxPlace.last_visit_date,
-                FirefoxOrigin.alt_frecency,
-                FirefoxOrigin.frecency,
-                FirefoxOrigin.host,
-                FirefoxOrigin.prefix,
-                FirefoxOrigin.recalc_alt_frecency,
-                FirefoxOrigin.recalc_frecency,
-                FirefoxPlace.alt_frecency,
-                FirefoxPlace.frecency,
-                FirefoxPlace.guid,
-                FirefoxPlace.recalc_alt_frecency,
-                FirefoxPlace.recalc_frecency,
-                FirefoxPlace.preview_image_url,
-                FirefoxBookmark.position,
-                FirefoxPlace.rev_host,
-                FirefoxPlace.site_name,
-                FirefoxBookmark.sync_change_counter,
-                FirefoxBookmark.sync_status,
-                FirefoxPlace.typed,
-                FirefoxPlace.url_hash,
-                FirefoxPlace.visit_count,
-            ),
-            "TO": (
-                Bookmark.id,
-                Bookmark.title,
-                Bookmark.url,
-                Bookmark.description,
-                Bookmark.type,
-                Bookmark.parent,
-                Bookmark.place_id,
-                Bookmark.origin_id,
-                Bookmark.date_added,
-                Bookmark.folder_type,
-                Bookmark.foreign_count,
-                Bookmark.guid,
-                Bookmark.hidden,
-                Bookmark.keyword_id,
-                Bookmark.last_modified,
-                Bookmark.last_visit_date,
-                Bookmark.origin_alt_frecency,
-                Bookmark.origin_frecency,
-                Bookmark.origin_host,
-                Bookmark.origin_prefix,
-                Bookmark.origin_recalc_alt_frecency,
-                Bookmark.origin_recalc_frecency,
-                Bookmark.place_alt_frecency,
-                Bookmark.place_frecency,
-                Bookmark.place_guid,
-                Bookmark.place_recalc_alt_frecency,
-                Bookmark.place_recalc_frecency,
-                Bookmark.preview_image_url,
-                Bookmark.position,
-                Bookmark.rev_host,
-                Bookmark.site_name,
-                Bookmark.sync_change_counter,
-                Bookmark.sync_status,
-                Bookmark.typed,
-                Bookmark.url_hash,
-                Bookmark.visit_count,
-            ),
+            "COMBINE": {
+                "FROM": (
+                    FirefoxBookmark.id,
+                    FirefoxBookmark.title,
+                    FirefoxPlace.url,
+                    FirefoxPlace.description,
+                    FirefoxBookmark.type,
+                    FirefoxBookmark.parent,
+                    FirefoxBookmark.fk,
+                    FirefoxPlace.origin,
+                    FirefoxBookmark.date_added,
+                    FirefoxBookmark.folder_type,
+                    FirefoxPlace.foreign_count,
+                    FirefoxBookmark.guid,
+                    FirefoxPlace.hidden,
+                    FirefoxBookmark.keyword_id,
+                    FirefoxBookmark.last_modified,
+                    FirefoxPlace.last_visit_date,
+                    FirefoxOrigin.alt_frecency,
+                    FirefoxOrigin.frecency,
+                    FirefoxOrigin.host,
+                    FirefoxOrigin.prefix,
+                    FirefoxOrigin.recalc_alt_frecency,
+                    FirefoxOrigin.recalc_frecency,
+                    FirefoxPlace.alt_frecency,
+                    FirefoxPlace.frecency,
+                    FirefoxPlace.guid,
+                    FirefoxPlace.recalc_alt_frecency,
+                    FirefoxPlace.recalc_frecency,
+                    FirefoxPlace.preview_image_url,
+                    FirefoxBookmark.position,
+                    FirefoxPlace.rev_host,
+                    FirefoxPlace.site_name,
+                    FirefoxBookmark.sync_change_counter,
+                    FirefoxBookmark.sync_status,
+                    FirefoxPlace.typed,
+                    FirefoxPlace.url_hash,
+                    FirefoxPlace.visit_count,
+                ),
+                "TO": (
+                    Bookmark.id,
+                    Bookmark.title,
+                    Bookmark.url,
+                    Bookmark.description,
+                    Bookmark.type,
+                    Bookmark.parent,
+                    Bookmark.place_id,
+                    Bookmark.origin_id,
+                    Bookmark.date_added,
+                    Bookmark.folder_type,
+                    Bookmark.foreign_count,
+                    Bookmark.guid,
+                    Bookmark.hidden,
+                    Bookmark.keyword_id,
+                    Bookmark.last_modified,
+                    Bookmark.last_visit_date,
+                    Bookmark.origin_alt_frecency,
+                    Bookmark.origin_frecency,
+                    Bookmark.origin_host,
+                    Bookmark.origin_prefix,
+                    Bookmark.origin_recalc_alt_frecency,
+                    Bookmark.origin_recalc_frecency,
+                    Bookmark.place_alt_frecency,
+                    Bookmark.place_frecency,
+                    Bookmark.place_guid,
+                    Bookmark.place_recalc_alt_frecency,
+                    Bookmark.place_recalc_frecency,
+                    Bookmark.preview_image_url,
+                    Bookmark.position,
+                    Bookmark.rev_host,
+                    Bookmark.site_name,
+                    Bookmark.sync_change_counter,
+                    Bookmark.sync_status,
+                    Bookmark.typed,
+                    Bookmark.url_hash,
+                    Bookmark.visit_count,
+                ),
+            },
+            "SEPARATE": {
+                "FROM": (
+                    Bookmark.id,
+                    Bookmark.title,
+                    # Bookmark.url,
+                    # Bookmark.description,
+                    Bookmark.type,
+                    Bookmark.parent,
+                    Bookmark.place_id,
+                    # Bookmark.origin_id,
+                    Bookmark.date_added,
+                    Bookmark.folder_type,
+                    # Bookmark.foreign_count,
+                    Bookmark.guid,
+                    # Bookmark.hidden,
+                    Bookmark.keyword_id,
+                    Bookmark.last_modified,
+                    # Bookmark.last_visit_date,
+                    # Bookmark.origin_alt_frecency,
+                    # Bookmark.origin_frecency,
+                    # Bookmark.origin_host,
+                    # Bookmark.origin_prefix,
+                    # Bookmark.origin_recalc_alt_frecency,
+                    # Bookmark.origin_recalc_frecency,
+                    # Bookmark.place_alt_frecency,
+                    # Bookmark.place_frecency,
+                    # Bookmark.place_guid,
+                    # Bookmark.place_recalc_alt_frecency,
+                    # Bookmark.place_recalc_frecency,
+                    # Bookmark.preview_image_url,
+                    Bookmark.position,
+                    # Bookmark.rev_host,
+                    # Bookmark.site_name,
+                    Bookmark.sync_change_counter,
+                    Bookmark.sync_status,
+                    # Bookmark.typed,
+                    # Bookmark.url_hash,
+                    # Bookmark.visit_count,
+                ),
+                "TO": (
+                    FirefoxBookmark.id,
+                    FirefoxBookmark.title,
+                    # FirefoxPlace.url,
+                    # FirefoxPlace.description,
+                    FirefoxBookmark.type,
+                    FirefoxBookmark.parent,
+                    FirefoxBookmark.fk,
+                    # FirefoxPlace.origin,
+                    FirefoxBookmark.date_added,
+                    FirefoxBookmark.folder_type,
+                    # FirefoxPlace.foreign_count,
+                    FirefoxBookmark.guid,
+                    # FirefoxPlace.hidden,
+                    FirefoxBookmark.keyword_id,
+                    FirefoxBookmark.last_modified,
+                    # FirefoxPlace.last_visit_date,
+                    # FirefoxOrigin.alt_frecency,
+                    # FirefoxOrigin.frecency,
+                    # FirefoxOrigin.host,
+                    # FirefoxOrigin.prefix,
+                    # FirefoxOrigin.recalc_alt_frecency,
+                    # FirefoxOrigin.recalc_frecency,
+                    # FirefoxPlace.alt_frecency,
+                    # FirefoxPlace.frecency,
+                    # FirefoxPlace.guid,
+                    # FirefoxPlace.recalc_alt_frecency,
+                    # FirefoxPlace.recalc_frecency,
+                    # FirefoxPlace.preview_image_url,
+                    FirefoxBookmark.position,
+                    # FirefoxPlace.rev_host,
+                    # FirefoxPlace.site_name,
+                    FirefoxBookmark.sync_change_counter,
+                    FirefoxBookmark.sync_status,
+                    # FirefoxPlace.typed,
+                    # FirefoxPlace.url_hash,
+                    # FirefoxPlace.visit_count,
+                ),
+            },
         }
 
     def connect(
@@ -111,6 +194,12 @@ class FirefoxBookmarks:
         """
 
         # Connect old models
+        from .models import database_obj
+        self._places_database = database_obj
+        self._places_path = locate_db(
+            look_under_path=look_under_path,
+            criterion=criterion,
+        )
         connect_firefox_models(
             look_under_path=look_under_path,
             criterion=criterion,
@@ -119,14 +208,19 @@ class FirefoxBookmarks:
         # Connect new model
         self._database = connect_bookmark_model(db_path=self._db_path)
 
-        # region Insert data into temp database
+        # Insert data into duplicate database
+        self._load()
+
+    def _load(self):
+        """Inserts data from places.sqlite to our duplicate bookmarks.sqlite database"""
+
         max_id = FirefoxBookmark.select(fn.MAX(FirefoxBookmark.id)).scalar()
 
         with self._database.atomic():
             for idx in range(0, max_id, BATCH_SIZE):
                 try:
                     selected = FirefoxBookmark.select(
-                        *(self._TRANSLATION["FROM"]))
+                        *(self._TRANSLATION["COMBINE"]["FROM"]))
 
                     joined = selected.join(
                         FirefoxPlace,
@@ -154,14 +248,13 @@ class FirefoxBookmarks:
 
                 Bookmark.insert_many(
                     source,
-                    fields=self._TRANSLATION["TO"],
+                    fields=self._TRANSLATION["COMBINE"]["TO"],
                 ).execute()
-        # endregion
 
     def bookmarks(
         self,
         *,
-        fields: list[FieldOrModel] = [],
+        fields: Iterable[FieldOrModel] = [],
         query: Expression | None = None,
     ) -> Iterable[Bookmark]:
         final_query: Expression = (Bookmark.type == BOOKMARK_TYPE)
@@ -173,7 +266,7 @@ class FirefoxBookmarks:
     def folders(
         self,
         *,
-        fields: list[FieldOrModel] = [],
+        fields: Iterable[FieldOrModel] = [],
         query: Expression | None = None,
     ) -> Iterable[Bookmark]:
         final_query: Expression = (Bookmark.type == FOLDER_TYPE)
@@ -182,9 +275,50 @@ class FirefoxBookmarks:
 
         return Bookmark.select(*fields).where(final_query).execute()
 
+    def update(
+        self,
+        *,
+        query: Expression | None = None,
+        data: dict[Field, Any],
+    ) -> int:
+        return Bookmark.update(data).where(query).execute()
+
+    def commit(self):
+        self._back_up_places()
+
+        max_id = FirefoxBookmark.select(fn.MAX(Bookmark.id)).scalar()
+
+        with self._places_database.atomic():
+            raise NotImplementedError
+
+    def _back_up_places(self):
+        file_name = f"backup-{int(time())}.sqlite"
+        dest = os.path.join(os.path.dirname(self._places_path), file_name)
+        shutil.copy(self._places_path, dest)
+
     def disconnect(self):
         self._database.close()
         os.remove(self._db_path)
+
+    def restore_latest_backup(self):
+        """Finds the latest backup and copies it to places.sqlite"""
+
+        dir_path, timestamps = self._get_backups()
+        backup_path = os.path.join(
+            dir_path,
+            f"backup-{max(timestamps)}.sqlite",
+        )
+        shutil.copy(backup_path, self._places_path)
+
+    def _get_backups(self) -> tuple[str, list[int]]:
+        dir_path = os.path.dirname(self._places_path)
+        files = os.listdir(dir_path)
+        timestamps = [
+            int(file.split('-')[1].split('.')[0]) for file in files
+            if file.startswith('backup-') and file.endswith('.sqlite')
+        ]
+
+        return dir_path, timestamps
 
 
 __all__ = [
